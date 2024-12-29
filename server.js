ef68365ba126d15723da4a0bc4bcef9ee9feb34b
Nur Azis Saputra <nurazissaputra@gmail.com>
2024-12-29 20:06:28 +0700

    fix: Database connection error closed

--- server.js
+++ server.js
@@ -15,19 +15,14 @@ app.use(express.json());
  * 
  * ============================================
  */
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to database:", err);
-    return;
-  }
-  console.log("Connected to database");
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 /**
